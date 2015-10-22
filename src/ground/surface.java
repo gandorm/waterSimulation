a8f9119c0cf8a34947d728b7da4d8c69b48c87c9
durden2 <gandor.michal@gmail.com>
2015-10-22 22:15:28 +0200

    start shit

--- src/ground/surface.java
+++ src/ground/surface.java
@@ -5,6 +5,7 @@ public class surface {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Pawel ma malego");
+		int[][] surface = new int[200][200];
 	}
 
 }

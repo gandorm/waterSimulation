d6cbe37eeb75525b85e3eeaafa694d1bd1f33cf6
Michal <Michal@Michal-PC>
2015-12-12 18:43:04 +0100

    Lepsza ukazanie symulacji morza

--- src/gerstner/Parameters.java
+++ src/gerstner/Parameters.java
@@ -3,14 +3,14 @@ package gerstner;
 public abstract class Parameters {
 
 	// Grid Params
-	public static final int GRID_SIZE_X = 256;
-	public static final int GRID_SIZE_Y = 256;
+	public static final int GRID_SIZE_X = 600;
+	public static final int GRID_SIZE_Y = 600;
 
 	// Gerstner Params
-	public static final int SIMULATION_TIME = 64;
+	public static final int SIMULATION_TIME = 20;
 
 	// Display Params
-	public static final int GRID_DISPLAY_MULTIPLIER = 3;
-	public static final double TIME_PASS = 0.013;				// the smaller the slower
+	public static final int GRID_DISPLAY_MULTIPLIER = 8;
+	public static final double TIME_PASS = 0.01;				// the smaller the slower
 
 }
